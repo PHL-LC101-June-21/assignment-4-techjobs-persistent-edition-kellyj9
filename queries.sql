eda3fb93b3416b69006d4d9d599bd6d4bf01dc50
Kelly Jones <kellymj6@gmail.com>
2021-12-07 01:58:16 -0500

    Modified JobRepository fields and modified HomeController methods

--- queries.sql
+++ queries.sql
@@ -5,8 +5,9 @@
 -- skills          varchar(255)
 
 -- Part 2: Test it with SQL
-Select name FROM employer WHERE location = "St. Louis City";
+SELECT name FROM employer WHERE location = "St. Louis City";
 
 -- Part 3: Test it with SQL
+DROP TABLE job;
 
 -- Part 4: Test it with SQL
